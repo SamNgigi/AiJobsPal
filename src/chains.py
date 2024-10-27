568bc29d5782bd6ca6d2cca794e891e790ab7eb1
SamNgigi <sam.t.ngigi@gmail.com>
2024-10-27 17:38:33 +0300

    [chore]: inital setup and poc

---
+++ src/chains.py
@@ -0,0 +1,62 @@
+import os
+
+from langchain_groq import ChatGroq 
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_CLOUD"), model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE
+            {page_data}
+            ### INSTRUCTION: 
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the
+            following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE)
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input = {'page_data' : cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Content too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION
+            You are Neo, a talented Machine Learning and software engineer dedicated to seamliess integration
+            of business process with A.I and machine learning tools to drive revenue and reduce cost.
+            Over our experience, we have empowered numeroud enterprises with tailored solutions fostering,
+            scalability, process optimization, cost reduction and heightened overral efficiency.
+            Your job is to write a cold email to potention employeers and clients regarding the job mentioned above
+            describing you capabilities in fulfilling their needs.
+            Also add the most work from the following links to showcase your abilities: {link_lst}
+            Remember you are Neo a talented Machine Learning and Software Engineer.
+            Do not provide preamble.
+            ### EMAIL (NO PREAMBLE):
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description" : str(job), "link_lst": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_CLOUD"))
+
+
